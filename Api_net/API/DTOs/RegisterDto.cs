761d5f5e3c433d084a295120d2a573782933818e
Valentyn Dementiev <gerold122mail@gmail.com>
2024-05-16 16:04:55 +0300

    API created, registration of user was finished and made available (w/o any validation, will be part of future improvements of app)

---
+++ Api_net/API/DTOs/RegisterDto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.DTOs
+{
+    public class RegisterDto
+    {
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        [StringLength(25, MinimumLength = 4)]
+        public string Password { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string KnownAs { get; set; }
+        public DateTime Created { get; set; } = DateTime.UtcNow;
+        public string Gender { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+    }
+}
