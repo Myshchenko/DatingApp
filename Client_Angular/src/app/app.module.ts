c10f51a83a7f923cd00799ea13299693b324f576
Valentyn Dementiev <gerold122mail@gmail.com>
2024-05-17 23:40:07 +0300

    nevermind, some fixes

--- Client_Angular/src/app/app.module.ts
+++ Client_Angular/src/app/app.module.ts
@@ -16,7 +16,6 @@ import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
 import { ToastrModule } from 'ngx-toastr';
 import { SharedModule } from './_modules/shared.module';
-import { TestErrorComponent } from './errors/test-error/test-error.component';
 import { ErrorInterceptor } from './_interceptors/error.interceptor';
 import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { ServerErrorComponent } from './errors/server-error/server-error.component';
@@ -31,7 +30,6 @@ import { ServerErrorComponent } from './errors/server-error/server-error.compone
     MemberDetailComponent,
     ListsComponent,
     MessagesComponent,
-    TestErrorComponent,
     NotFoundComponent,
     ServerErrorComponent
   ],
