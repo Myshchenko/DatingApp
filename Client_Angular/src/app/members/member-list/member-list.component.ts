ae496feb4beb72aa1154b17f3b1a0e2a938e58ae
Valentyn Dementiev <gerold122mail@gmail.com>
2024-05-24 02:29:23 +0300

    filtering by age and gender was added to application

--- Client_Angular/src/app/members/member-list/member-list.component.ts
+++ Client_Angular/src/app/members/member-list/member-list.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { take } from 'rxjs';
 import { Member } from 'src/app/_models/member';
+import { Pagination } from 'src/app/_models/pagination';
+import { User } from 'src/app/_models/user';
+import { UserParams } from 'src/app/_models/userParams';
+import { AccountService } from 'src/app/_services/account.service';
 import { MembersService } from 'src/app/_services/members.service';
 
 @Component({
@@ -8,9 +13,22 @@ import { MembersService } from 'src/app/_services/members.service';
   styleUrls: ['./member-list.component.css']
 })
 export class MemberListComponent implements OnInit{
-  members: Member[] = []
+  members: Member[] = [];
+  pagination: Pagination | undefined;
+  userParams : UserParams | undefined;
+  user : User | undefined;
+  genderList = [{value: 'male', display: 'Males'}, {value: 'female', display: 'Females'}]
 
-  constructor(private memberService: MembersService) {
+  constructor(private memberService: MembersService, private accountService : AccountService) {
+    this.accountService.currentUser$.pipe(take(1)).subscribe({
+      next: user => {
+        if (user){
+          this.userParams = new UserParams(user);
+          console.log(this.userParams);  
+          this.user = user; 
+        }
+      }
+    })
   }
 
   ngOnInit(): void {
@@ -18,8 +36,28 @@ export class MemberListComponent implements OnInit{
   }
 
   loadMembers(){
-    this.memberService.getMembers().subscribe({
-      next: members => this.members = members
+    if(!this.userParams) return;
+    this.memberService.getMembers(this.userParams).subscribe({
+      next: response => {
+        if (response.result && response.pagination){
+          this.members = response.result;
+          this.pagination = response.pagination;
+        }
+      }
     })
+  }
+
+  resetFilters(){
+    if(this.user){
+      this.userParams = new UserParams(this.user);
+      this.loadMembers();
+    }
+  }
+
+  pageChanged(event: any){
+    if(this.userParams && this.userParams?.pageNumber !== event.page){
+      this.userParams.pageNumber = event.page;
+      this.loadMembers();
+    }
   }
 }
