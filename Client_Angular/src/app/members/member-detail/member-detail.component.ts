a494d90b85b255808357090cd8081e618706845a
Valentyn Dementiev <gerold122mail@gmail.com>
2024-06-15 14:30:18 +0300

    added like button for profile details

--- Client_Angular/src/app/members/member-detail/member-detail.component.ts
+++ Client_Angular/src/app/members/member-detail/member-detail.component.ts
@@ -4,6 +4,7 @@ import { ActivatedRoute } from '@angular/router';
 import { GalleryItem, GalleryModule, ImageItem } from 'ng-gallery';
 import { TabsModule } from 'ngx-bootstrap/tabs';
 import { TimeagoModule, TimeagoPipe } from 'ngx-timeago';
+import { ToastrService } from 'ngx-toastr';
 import { Member } from 'src/app/_models/member';
 import { MembersService } from 'src/app/_services/members.service';
 import { PresenceService } from 'src/app/_services/presence.service';
@@ -19,7 +20,8 @@ export class MemberDetailComponent implements OnInit {
   member: Member | undefined;
   images: GalleryItem[] = []
 
-  constructor(private memberService: MembersService, private route: ActivatedRoute, public presenceService: PresenceService) {
+  constructor(private membersService: MembersService, private route: ActivatedRoute,
+    public presenceService: PresenceService, private toastr : ToastrService) {
 
   }
 
@@ -30,7 +32,7 @@ export class MemberDetailComponent implements OnInit {
   loadMember(){
     const username = this.route.snapshot.paramMap.get('username');
     if(!username) return;
-    this.memberService.getMember(username).subscribe({
+    this.membersService.getMember(username).subscribe({
       next: member => {
         this.member = member,
         this.getImages()
@@ -46,5 +48,27 @@ export class MemberDetailComponent implements OnInit {
         thumb: photo.url
       }))
     }
+  }
+
+  addLike(member : Member){
+    this.membersService.addLike(member.userName).subscribe({
+      next: () => {
+        this.toastr.success('You have liked ' + member.knownAs);
+        this.membersService.likedUsernames.push(member.userName);
+      }
+    })
+  }
+
+  deleteLike(member : Member){
+    this.membersService.deleteLike(member.userName).subscribe({
+      next: () => {
+        this.toastr.success('The like has been removed for ' + member.knownAs);
+        this.membersService.getLikedUsernames();
+      }
+    })
+  }
+
+  isLiked(username: string) : boolean{
+    return this.membersService.likedUsernames.includes(username.toLowerCase());
   }
 }
